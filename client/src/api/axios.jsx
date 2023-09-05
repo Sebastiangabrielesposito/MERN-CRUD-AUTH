709596a454d87a0165c4e7e35eb40ac808534f22
sebastian__gabriel <espositosebastian@hotmail.com>
2023-09-05 17:23:37 -0300

    Uploading

--- client/src/api/axios.jsx
+++ client/src/api/axios.jsx
@@ -4,6 +4,6 @@ const instance = axios.create({
     baseURL: 
     'https://mern-crud-auth-server-ten.vercel.app/api' ,
     // 'http://localhost:4000/api',
-    withCredentials: true,
+    // withCredentials: true,
   });
 export default instance
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,4 @@
-// import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-  //   plugins: [react()],
-  // })
-// import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
