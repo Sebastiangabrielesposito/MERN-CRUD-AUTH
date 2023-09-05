fa2c54a472672335cec9c1f43cccf18905271248
sebastian__gabriel <espositosebastian@hotmail.com>
2023-09-05 14:44:57 -0300

    Uploading

--- client/src/api/axios.jsx
+++ client/src/api/axios.jsx
@@ -1,8 +1,9 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: 'mern-crud-auth-nine.vercel.app' ,
-    // 'http://localhost:4000/api',
+    baseURL: 
+    // 'mern-crud-auth-nine.vercel.app' ,
+    'http://localhost:4000/api',
     withCredentials: true,
   });
 export default instance
