e3783f214d66402c830d83da028cddc37da4c355
sebastian__gabriel <espositosebastian@hotmail.com>
2023-09-05 13:34:47 -0300

    Uploading MERN-CRUD-AUTH

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,26 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+// // import { defineConfig } from 'vite'
+// // import react from '@vitejs/plugin-react'
+
+// // // https://vitejs.dev/config/
+// // export default defineConfig({
+// //   plugins: [react()],
+// // })
+// import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
+// export default {
+//   plugins: [reactRefresh()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:4000',
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, ''),
+//       },
+//     },
+//   },
+// };
+
 import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
@@ -17,6 +33,11 @@ export default {
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['zod'], // Añade 'zod' como módulo externo
     },
   },
 };
