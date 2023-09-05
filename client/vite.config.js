867bd0421c97af6cb6b8bc8de597adcf4684e03d
sebastian__gabriel <espositosebastian@hotmail.com>
2023-09-05 16:16:21 -0300

    Uploading

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,26 @@
-// // import { defineConfig } from 'vite'
-// // import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// // // https://vitejs.dev/config/
-// // export default defineConfig({
-// //   plugins: [react()],
-// // })
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+// import reactRefresh from '@vitejs/plugin-react-refresh';
+
+// https://vitejs.dev/config/
+export default {
+  plugins: [reactRefresh()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+};
+
 // import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // // https://vitejs.dev/config/
@@ -19,27 +35,11 @@
 //       },
 //     },
 //   },
+//   build: {
+//     rollupOptions: {
+//       external: ['zod'], 
+//     },
+//     // outDir: './dist', 
+//   },
+//   base: '/client/',
 // };
-
-import reactRefresh from '@vitejs/plugin-react-refresh';
-
-// https://vitejs.dev/config/
-export default {
-  plugins: [reactRefresh()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
-  build: {
-    rollupOptions: {
-      external: ['zod'], 
-    },
-    // outDir: './dist', 
-  },
-  base: '/client/',
-};
